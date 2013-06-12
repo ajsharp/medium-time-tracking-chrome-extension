3a32c6063968bb10c38ecdf494910e51be8a722e
Alex Sharp <ajsharp@gmail.com>
2013-06-12 14:39:03 -0700

    check for state changes by polling every 1 second

--- tracking.js
+++ tracking.js
@@ -18,9 +18,35 @@ jQuery(document).ready(function($) {
     })
   }
 
+  var currentState = null
+
   post.title = $('h1.post-title').html()
-  post.id = $('article').attr('data-post-id')
-  track('in')
+  post.id    = $('article').attr('data-post-id')
+
+  // set initial edit / not editing state
+  if (window.location.href.match(/edit$/)) {
+    currentState = 'editing'
+    track('in')
+  } else {
+    currentState = 'not editing'
+  }
+
+  var checkStateChange = function() {
+    if (window.location.href.match(/edit$/)) {
+      if (currentState == 'not editing') {
+        currentState = 'editing'
+        track('in')
+      }
+    } else {
+      if (currentState == 'editing') {
+        currentState = 'not editing'
+        track('out')
+      }
+    }
+  }
+
+  // check if the user started editing every second
+  window.setInterval(checkStateChange, 1000)
 
   $(window).unload(function(e) {
     track("out")
